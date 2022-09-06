a3a04c01c6ba152fff9bec59191cf27b1e5fa1f2
Muhammad Umer <thisismumer@gmail.com>
2022-09-07 01:43:33 +0500

    Reduced default batch size
    Previously crashed colab GPU session.

--- config/default.py
+++ config/default.py
@@ -14,7 +14,7 @@ def get_config():
     config = ml_collections.ConfigDict()
 
     config.learning_rate = 3.5e-4
-    config.batch_size = 128
+    config.batch_size = 64
     config.num_epochs = 10
 
     return config
